5e4c1c98470369ad607f32333c7ba135770aa42f
Mohamad Hasan Khajedehi <khajedehi.hasan@gmail.com>
2026-05-12 13:49:57 +0200

    add frontend and logic

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://127.0.0.1:8000/api";
+  process.env.NEXT_PUBLIC_API_BASE_URL || "https://betalab-raci.onrender.com/api";
 
 async function request<T>(
   path: string,
